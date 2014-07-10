73ea2ccaf1ab3f44ed4d1180d1ceadca9f2edd39
Adam Schubert <feartohell@seznam.cz>
2014-07-10 02:25:54 +0200

    Call right classes

--- git-deploy/__main__.py
+++ git-deploy/__main__.py
@@ -7,6 +7,7 @@ __date__ ="$6.7.2014 2:01:34$"
 
 import git_deploy
 import git_deploy_remote
+import git_deploy_server
 import sys
 import socket
 import fileinput
@@ -14,23 +15,24 @@ import getpass
 import os
 
 if __name__ == "__main__":
-  stdin = fileinput.input()[0].strip().split(' ')
-  #stdin 3 items thats post-receive
-  if len(stdin) == 3:
-    prev, current, branch = stdin
-    git_user = getpass.getuser()
-    repository_path = os.path.join('/home/', git_user, '/repositories/')
-
-    #Build needed info
-    ssh_path = git_user + '@' + socket.gethostname() + ':' + os.getcwd().replace(repository_path,'')
-    git_deploy_remote.GitDeployRemote(current, branch, ssh_path, tmp_path)
-  elif sys.argv[1] == 'server':
-    pass
+  if sys.argv[1] == 'server':
+    git_deploy_server.GitDeployServer()
   elif len(sys.argv) == 1:
     #one arg means local run
-    git_deploy.GitDeploy()
+    git_deploy.GitDeploy().get_log().output()
   elif len(sys.argv) == 2:
     #two args means local run
-    git_deploy.GitDeploy(sys.argv[1])
+    git_deploy.GitDeploy(sys.argv[1]).get_log().output()
+  else:
+    stdin = fileinput.input()[0].strip().split(' ')
+    #stdin 3 items thats post-receive
+    if len(stdin) == 3:
+      prev, current, branch = stdin
+      git_user = getpass.getuser()
+      repository_path = os.path.join('/home/', git_user, '/repositories/') #FIXME GET REPO PATH FROM CONFIG
+
+      #Build needed info
+      ssh_path = git_user + '@' + socket.gethostname() + ':' + os.getcwd().replace(repository_path,'')
+      git_deploy_remote.GitDeployRemote(current, branch, ssh_path, tmp_path) #FIXME TMP PATH GET FROM CONFIG
   
   
