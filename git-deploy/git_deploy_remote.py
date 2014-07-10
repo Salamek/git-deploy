a3ebc028657f32579da7f09a78534a35c8344133
Adam Schubert <feartohell@seznam.cz>
2014-07-10 02:24:42 +0200

    Put deploying into thread

--- git-deploy/git_deploy_remote.py
+++ git-deploy/git_deploy_remote.py
@@ -5,97 +5,82 @@
 __author__="Adam Schubert"
 __date__ ="$6.7.2014 0:22:44$"
 
-import time
 import os
 import git_deploy
+import threading
 from classes import Git
 from classes import Shell
 
-class GitDeployRemote:
-  ssh_path = None
-  branch = None
-  lock_file = 'deploy.lck'
-  current_revision = None
-  tmp = None
-  git = None
+class DeployWorker(threading.Thread):
+  caller = None
+  def __init__(self, caller):
+    super(DeployWorker, self).__init__()
+    self.caller = caller
+    
+  work_list = []
+  def run(self):
+    while(len(self.work_list) > 0):
+      current, branch, ssh_path, tmp = self.work_list.pop()
+      try:
+        self.sync(tmp, branch, ssh_path)
+        self.caller.loger(self.deploy(tmp))
+      except Exception as e:
+        self.caller.fail(str(e))
+        
+  
+  def add_work(self, work):
+    self.work_list.append(work)
+    
+  """
+   * Method sync local TMP with main repo
+  """
+  def sync(self, tmp, branch, ssh_path):
+    git = Git(tmp)
+    git.update(branch, ssh_path)
+    
+  """
+   * Method calls local deployer
+   * @throws Exception
+  """
+  def deploy(self, tmp):
+    return git_deploy.GitDeploy(tmp).get_log()
 
+
+
+
+class GitDeployRemote:
   """
    * Constructor
    * @param string $stdin STDIN
   """
   def __init__(self, current, branch, ssh_path, tmp_path):
-    
-    #Build needed info
-    self.ssh_path = ssh_path
-    print(Shell.color('Path to repository is: ' + self.ssh_path, 'white', 'black'))
-    #Parse stdin
-
-    self.current_revision = current
-    self.branch = branch.split('/')[-1]
+    branch = branch.split('/')[-1]
 
     #Separate tmp repos per branch
     parsed_path = Git.git_url_parse(ssh_path)
-    self.tmp = os.path.join(tmp_path, parsed_path['hostname'], parsed_path['path'], self.branch)
-    self.git = Git(self.tmp)
-    try:
-      self.running_job()
-      self.sync()
-      self.deploy()
-    except Exception as e:
-      print(Shell.color(str(e), 'white', 'red'))
+    tmp = os.path.join(tmp_path, parsed_path['hostname'], parsed_path['path'], branch)
+    
+    
+    workers = {}
+    
+    #tmp is uniqe identifier of repo, this creates front for each repo
+    if tmp in workers:
+      w = workers[tmp]
+      w.add_work([current, branch, ssh_path, tmp])
+      if w.isAlive() == False:
+        w.start()
+    else:
+      workers[tmp] = DeployWorker(self)
+      workers[tmp].add_work([current, branch, ssh_path, tmp])
+      workers[tmp].start()
+      
+    #clean not running workers
+    for tmp in workers:
+      if workers[tmp].isAlive() == False:
+        del workers[tmp]
 
-  """
-   * Method checks if there is running job for branch, if is it will sleep till another job ends
-  """
-  def running_job(self):
-    wait_time = 10#s
-    while self.check_work() == True:
-      print(Shell.color('Another deploy job is running, waiting {} s to try again...'.format(wait_time), 'yellow', 'black'))
-      time.sleep(wait_time)
+  def loger(self, l):
+    l.output()
 
-  """
-   * Method checks if there is unfinished job
-   * @return boolean
-  """
-  def check_work(self):
-    lock_file_path = os.path.join(self.tmp, self.lock_file)
-    if os.path.isfile(lock_file_path):
-      #Lock file is less then one hour old... let it be and wait till expire or get removed by finished job
-      if os.path.getctime(lock_file_path) + 3600 > int(time.time()):
-        return True
-    return False
-
-  """
-   * Method sync local TMP with main repo
-  """
-  def sync(self):
-    self.git.update(self.branch, self.ssh_path)
-
-    #Create own lock file and continue
-    f = open(os.path.join(self.tmp, self.lock_file), 'w')
-    f.write(self.current_revision)
-    f.close()
-
-  """
-   * Method calls local deployer
-   * @throws Exception
-  """
-  def deploy(self):
-    git_deploy.GitDeploy(self.tmp)
-    self.destroy_lock()
-
-  """
-   * Method destroys lock file
-  """
-  def destroy_lock(self):
-    lock_file_path = os.path.join(self.tmp, self.lock_file)
-    if self.lock_file and os.path.isfile(lock_file_path):
-      os.unlink(lock_file_path);
-
-  """
-   * Desctructort
-  """
-  def __del__(self):
-    self.destroy_lock()
-  
-  
+  def fail(self, fail):
+    Shell.color(fail, 'white', 'red')
