df73c1d06f6ee540416adf3edf05123af2f56399
Adam Schubert <feartohell@seznam.cz>
2014-07-10 13:35:12 +0200

    Added GNU header

--- git-deploy/git_deploy.py
+++ git-deploy/git_deploy.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python2
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__="Adam Schubert"
+__date__ ="$6.7.2014 2:01:34$"
+
 
 import os
 import ConfigParser
@@ -25,11 +42,13 @@ class GitDeploy:
     self.currentRevision = None
     root = None
     
-    if isinstance(config, str):
+    if config :
       if os.path.isdir(config):
         root = config
       else:
         raise Exception(config + ' is not a directory or dont exists!')
+    else:
+      root = None
 
     try:
       self.git = Git(root);
@@ -156,8 +175,8 @@ class GitDeploy:
         except Exception as e:
           self.log.add(str(e), 'error')
 
-      connection.upload_string(os.path.join(self.config['uri'].path, self.revison_file), git_revision_log);
-
+      #connection.upload_string(os.path.join(self.config['uri'].path, self.revison_file), git_revision_log);
+      self.log.add('Deploy done!', 'ok')
     else:
       self.log.add('Revisions match, no deploy needed.', 'ok')
       
