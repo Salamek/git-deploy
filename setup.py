1711b40d683d24ae3ba9b445a05b0c07a7cba122
Adam Schubert <adam.schubert@sg1-game.net>
2014-07-10 18:30:41 +0200

    Fixes for deb build

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ __date__ ="$8.7.2014 20:26:41$"
 
 setup(
   name="git-deploy",
-  version="2.0.0",
+  version="2.0.2",
   author=__author__,
   author_email="adam.schubert@sg1-game.net",
   description="Git-deploy is tool written in python to allow fast and easy deployments on remote servers wia S/FTP, SSH/SCP",
@@ -17,11 +17,11 @@ setup(
   license="GPL",
   install_requires=['paramiko', 'twisted'],
   url="https://github.com/Salamek/git-deploy",
-  packages=['git-deploy', 'git-deploy/classes'],
-  package_dir={'git-deploy': 'git-deploy'},
+  packages=['git_deploy', 'git_deploy/classes'],
+  package_dir={'git_deploy': 'git_deploy'},
   #package_data={},
   entry_points={
-    'console_scripts': ['git-deploy']
+    'console_scripts': ['git-deploy = git_deploy:main']
   },
   data_files=[
     ('/etc/init.d', ['etc/init.d/git-deploy']), ('/etc/git-deploy', ['etc/git-deploy/git-deploy.cfg'])
