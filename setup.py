1adeb59f5cfc832daecfee6ac7eb2c848604552e
Adam Schubert <feartohell@seznam.cz>
2014-07-14 00:37:30 +0200

    Set file interpreter

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+#!/usr/bin/env python2
 
 from setuptools import setup
 
