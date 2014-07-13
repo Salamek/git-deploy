1aa5584f1e6a3637160918f3037a3128254a0cc7
Adam Schubert <feartohell@seznam.cz>
2014-07-14 01:01:22 +0200

    Setup for init based systems and for systemd based systems

---
+++ setup-systemd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+__author__="Adam Schubert"
+__date__ ="$8.7.2014 20:26:41$"
+
+setup(
+  name="git-deploy",
+  version="2.0.5",
+  author=__author__,
+  author_email="adam.schubert@sg1-game.net",
+  description="Git-deploy is tool written in python to allow fast and easy deployments on remote servers wia S/FTP, SSH/SCP",
+  long_description=open('README.md').read(),
+  license="GPL",
+  install_requires=['paramiko', 'twisted', 'git'],
+  url="https://github.com/Salamek/git-deploy",
+  packages=['git_deploy', 'git_deploy/classes'],
+  package_dir={'git_deploy': 'git_deploy'},
+  #package_data={},
+  entry_points={
+    'console_scripts': ['git-deploy = git_deploy:main']
+  },
+  data_files=[
+    ('/usr/lib/systemd/system/', ['usr/lib/systemd/system/git-deploy.service']), ('/etc/git-deploy', ['etc/git-deploy/git-deploy.cfg'])
+  ]
+)
